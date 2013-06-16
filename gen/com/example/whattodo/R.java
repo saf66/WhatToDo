e358dafe0b8f901bd869b9470d1e3edae9656084
Sean <saf66@case.edu>
2013-06-16 17:40:00 -0400

    custom ListAdapter

--- gen/com/example/whattodo/R.java
+++ gen/com/example/whattodo/R.java
@@ -24,10 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int bottomtext=0x7f080002;
+        public static final int checkBox1=0x7f080000;
+        public static final int toptext=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030000;
+        public static final int row=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,7 +39,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int empty=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int main_no_items=0x7f050004;
     }
     public static final class style {
         /** 
--- src/com/example/whattodo/MainActivity.java
+++ src/com/example/whattodo/MainActivity.java
@@ -1,16 +1,89 @@
 package com.example.whattodo;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+// http://www.softwarepassion.com/android-series-custom-listview-items-and-adapters/
+// http://stackoverflow.com/questions/11265743/onitemclicklistener-with-custom-adapter-and-listview
+public class MainActivity extends ListActivity implements OnItemClickListener,OnItemLongClickListener {
+   
+    private ProgressDialog m_ProgressDialog = null;
+    private ArrayList<Order> m_orders = null;
+    private OrderAdapter m_adapter;
+   
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        lv.setOnItemClickListener(this);
+        lv.setOnItemLongClickListener(this);
+        
+        m_orders = new ArrayList<Order>();
+        this.m_adapter = new OrderAdapter(this, R.layout.row, m_orders);
+        setListAdapter(this.m_adapter);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+		Thread thread = new Thread(null, new Runnable() {
+		    @Override
+		    public void run() {
+		        getOrders();
+		    }
+		});
+        thread.start();
+        m_ProgressDialog = ProgressDialog.show(MainActivity.this, "Please wait...", "Retrieving data ...", true);
+    }
+    
+    private Runnable returnRes = new Runnable() {
+
+        @Override
+        public void run() {
+            if(m_orders != null && m_orders.size() > 0){
+                m_adapter.notifyDataSetChanged();
+                for(int i=0;i<m_orders.size();i++)
+                m_adapter.add(m_orders.get(i));
+            }
+            m_ProgressDialog.dismiss();
+            m_adapter.notifyDataSetChanged();
+        }
+      };
+    
+    private void getOrders(){
+        try{
+            m_orders = new ArrayList<Order>();
+            Order o1 = new Order();
+            o1.setOrderName("SF services");
+            o1.setOrderStatus("Pending");
+            Order o2 = new Order();
+            o2.setOrderName("SF Advertisement");
+            o2.setOrderStatus("Completed");
+            m_orders.add(o1);
+            m_orders.add(o2);
+               Thread.sleep(2000);
+            Log.i("ARRAY", ""+ m_orders.size());
+          } catch (Exception e) {
+            Log.e("BACKGROUND_PROC", e.getMessage());
+          }
+          runOnUiThread(returnRes);
+      }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,4 +92,45 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Toast.makeText(getApplicationContext(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+		Toast.makeText(getApplicationContext(), "You long clicked on: " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+		return true;
+	}
+
+	private class OrderAdapter extends ArrayAdapter<Order> {
+
+	    private ArrayList<Order> items;
+
+	    public OrderAdapter(Context context, int textViewResourceId, ArrayList<Order> items) {
+	            super(context, textViewResourceId, items);
+	            this.items = items;
+	    }
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	            View v = convertView;
+	            if (v == null) {
+	                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	                v = vi.inflate(R.layout.row, null);
+	            }
+	            Order o = items.get(position);
+	            if (o != null) {
+	                    TextView tt = (TextView) v.findViewById(R.id.toptext);
+	                    TextView bt = (TextView) v.findViewById(R.id.bottomtext);
+	                    if (tt != null) {
+	                          tt.setText("Name: "+o.getOrderName());
+	                    }
+	                    if(bt != null){
+	                          bt.setText("Status: "+ o.getOrderStatus());
+	                    }
+	            }
+	            return v;
+	    }
+	}
 }
